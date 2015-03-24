138068fc1e80e2abaacbe7f6a2512675b8d097f1
Sean Haughton <seanphaughton@gmail.com>
2015-03-24 09:50:37 +0000

    Fixed index.ejs script 404 errors

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 app.set('view engine', 'ejs');
-app.use('/public', express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (request, response){
   response.render('index');
